acd5b64be31577f0501828ccfa557be85798c0c0
Leduong1 <99656475+Leduong1@users.noreply.github.com>
2024-10-20 20:34:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '';
+const giftUrl = 'https://www.facebook.com/thuthao54279';
+const eventName = 'GỬI TẶNG MAI THẢO😘😘🥰🥰🥰';
+const titleCard = 'Tặng MAI THẢO';
+const contentCard = 'Hélloo.. chúc ngày tốt lành, đây là năng lượng tích cực được gửi tới MAI THẢO ngày cuối tuần. Giống như hôm nay, ngày Phụ nữ Việt Nam. U là 20/10=10(điểm)Vậy nên nhân dịp, DuongLee xin được chúc u mãiii xinh đẹp lộng lẫy, dịu dàng thanh lịch, sang trọng quý phái, quyến rũ gợi cảm, ngọt ngào duyên dáng, thông minh trí tuệ, đảm đang tháo vát, khéo léo giỏi giang, kiên cường bản lĩnh, hài hước dí dỏm, lạc quan yêu đời, năng động tích cực…. 8386 mãi yêu mãi đỉnh nhenn ';
+
+
+const giftImage = 'a.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
